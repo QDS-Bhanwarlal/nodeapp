d3ef28b4eef63a5597e4d78abdfa3c6ad1a63688
bhanwar0607 <bhavar1011@gmail.com>
2019-12-17 11:45:15 +0530

    Added callback function for test, failed one test cases intentionaly for test

--- tests/users.test.js
+++ tests/users.test.js
@@ -4,30 +4,32 @@ const app = require('../app')
 const path = "localhost:3000";
 
 describe('Users', () => {
-    it('Should return all the users', () => {
+    it('Should return all the users', (done) => {
         request(path)
             .get('/users')
             .expect(200)
             .then(res => {
                 expect(res.body.success).toBe(true);
                 expect.arrayContaining(res.body.payload);
+                done();
             });
     })
 
 
     describe('Find User by Id', () => {
-        it('Should return user', () => {
+        it('Should return user', (done) => {
             request(path)
                 .get('/users/1')
                 .expect(200)
                 .then(res => {
                     expect(res.body.success).toBe(true);
                     expect(res.body.payload.length).toBe(1);
-                    expect(res.body.payload[0].id).toBe(1);
+                    expect(res.body.payload[0].id).toBe(2);
+                    done();
                 });
         })
 
-        it('Should return message user not found', () => {
+        it('Should return message user not found', (done) => {
             request(path)
                 .get('/users/9')
                 .expect(200)
@@ -35,11 +37,12 @@ describe('Users', () => {
                     expect(res.body.success).toBe(false);
                     expect.stringContaining(res.body.message);
                     expect(res.body.message).toBe("No user found User Id:9");
+                    done();
                 });
         })
     })
 
-    it('Should return users count', () => {
+    it('Should return users count', (done) => {
         request(path)
             .get('/users/count')
             .expect(200)
@@ -47,6 +50,7 @@ describe('Users', () => {
                 expect(res.body.success).toBe(true);
                 expect.arrayContaining(res.body.payload);
                 expect(res.body.payload.user_count);
+                done();
             });
     })
 })
